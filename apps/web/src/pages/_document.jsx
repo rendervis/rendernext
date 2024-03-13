2c859d061e85c30c27512bc87e8109f803df3c76
Octavian <rendervis333@gmail.com>
2024-03-13 10:47:05 +0200

    [new]:: favicon

--- apps/web/src/pages/_document.jsx
+++ apps/web/src/pages/_document.jsx
@@ -36,6 +36,14 @@ export default function Document() {
           type="application/feed+json"
           href={`${process.env.NEXT_PUBLIC_SITE_URL}/rss/feed.json`}
         />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff"></meta>
       </Head>
       <body className="flex h-full flex-col bg-zinc-50">
         <Main />
